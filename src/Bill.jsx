e9bec768993fe33e9ebec60d49f71608cdb47833
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2025-02-10 19:17:49 +0530

    made the addbox weel

--- src/Bill.jsx
+++ src/Bill.jsx
@@ -188,13 +188,13 @@ const Bill = () => {
                 onChange={handleItemChange}
               />
             </div>
-            <div className="input-block" style={{alignItems : "center"}}>
+            <div className="input-block" style={{ alignItems: "center" }}>
               <label htmlFor="" className="label">
                 Email -{" "}
               </label>
               <input
                 type="text"
-                style={{ width: "30vw"}}
+                style={{ width: "30vw" }}
                 className="input"
                 name="fromemail"
                 value={itemData.fromemail}
@@ -346,7 +346,16 @@ const Bill = () => {
         </button>
 
         {addOpen && (
-          <div className="addbox" style={{ padding: "2%" }}>
+          <div className="addbox">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+              fill="#fff"
+              className="closebtn"
+              onClick={() => setAddOpen(false)}
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
             <label
               htmlFor="description"
               className="label"
@@ -615,6 +624,7 @@ const StyledBill = styled.div`
     border-radius: 20px;
     margin-left: 25%;
     min-width: 40%;
+    padding: 3%;
   }
 
   #fromcontact {
@@ -633,6 +643,12 @@ const StyledBill = styled.div`
 
   #fromcontact2 {
     width: 15vw;
+  }
+
+  .closebtn {
+    background-color: #302f35;
+    margin-left: auto;
+    width: 25px;
   }
 
   @media screen and (max-width: 480px) {
@@ -671,6 +687,17 @@ const StyledBill = styled.div`
 
     #fromcontact2 {
       width: 20vw;
+    }
+
+    .addbox {
+      top: 25vh;
+      width: 80%;
+      margin-left: 2.5%;
+      padding: 5%;
+    }
+
+    .closebtn {
+      width: 20px;
     }
   }
 `;
